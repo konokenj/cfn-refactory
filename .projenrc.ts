f7488afd523517ff4bee1f572df616cc85fc62ee
Kenji Kono <konoken@amazon.co.jp>
2024-04-02 22:57:20 +0900

    fix bin

--- .projenrc.ts
+++ .projenrc.ts
@@ -7,6 +7,7 @@ const project = new typescript.TypeScriptProject({
   eslint: true,
   deps: ["chalk@4.1.2", "yargs", "@aws-sdk/client-cloudformation"],
   releaseToNpm: true,
+  npmProvenance: false, // https://github.com/projen/projen/issues/3479
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
