c8a420d2270d3b03c772bb07aaaf0eacb4e145a4
Kenji Kono <konoken@amazon.co.jp>
2024-04-02 17:46:07 +0900

    add projen

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,8 +1,11 @@
-import { typescript } from 'projen';
+import { typescript } from "projen";
 const project = new typescript.TypeScriptProject({
-  defaultReleaseBranch: 'main',
-  name: 'cloudformation-refactoring-helper',
+  defaultReleaseBranch: "main",
+  name: "cloudformation-refactoring-helper",
   projenrcTs: true,
+  prettier: true,
+  eslint: true,
+  deps: [],
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
